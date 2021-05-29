27074a6fcc11edb19cf70bb49c221da53997ccdc
CinisArboris <eyver.evm@gmail.com>
2021-05-29 11:21:37 -0400

    update : Extract Method

---
+++ longMethod.py
@@ -0,0 +1,21 @@
+# ---------------------------------------------
+def getPriceDetails():
+	print (" ...(data)...")
+fullName = "JohnWhick"
+idClient = "root-007"
+# .............................................
+def factura_no():
+	getPriceDetails()
+	print ("fullName", fullName)
+	print ("idClient", idClient)
+# .............................................
+def factura_si():
+	getPriceDetails()
+	getClientDetails()
+def getClientDetails():
+	print ("fullName", fullName)
+	print ("idClient", idClient)
+# ---------------------------------------------
+factura_no()
+print ("-------------------------------------")
+factura_si()
